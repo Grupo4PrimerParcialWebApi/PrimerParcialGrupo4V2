3854309ccb90960b56cbc22262084ad85f307351
Eiver Romero <a2024112722@estudiantes.upsa.edu.bo>
2025-09-29 20:27:50 -0400

    Creación AppDBContext.cs en Data

--- PrimerParcialGrupo4WebApi/Data/AppDbContext.cs
+++ PrimerParcialGrupo4WebApi/Data/AppDbContext.cs
@@ -1,6 +1,28 @@
-namespace PrimerParcialGrupo4WebApi.Data;
+using Microsoft.EntityFrameworkCore;
+using PrimerParcialGrupo4WebApi.Models;
 
-public class AppDbContext
+namespace PrimerParcialGrupo4WebApi.Data
 {
-    
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Registrar la entidad Event
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("dbo");
+
+            modelBuilder.Entity<Event>(e =>
+            {
+                e.ToTable("Events");
+                e.Property(ev => ev.Title).HasMaxLength(200).IsRequired();
+                e.Property(ev => ev.Location).HasMaxLength(200).IsRequired();
+                e.HasIndex(ev => new { ev.Title, ev.StartAt }); // Ejemplo de índice compuesto
+            });
+        }
+    }
 }
